e5cbaeafb8fa3494aa338d06bdedf255a6d86261
viktor.hajer <viktor.hajer@evosoft.com>
2022-11-07 13:34:01 +0100

    New version ++

--- src/app/model/station-color.model.ts
+++ src/app/model/station-color.model.ts
@@ -8,10 +8,16 @@ export const STATION_COLORS = [
   new StationColor('Gray', 'gray'),
   new StationColor('Dark Gray', 'dgray'),
   new StationColor('Red', 'red'),
+  new StationColor('Pink', 'pink'),
+  new StationColor('Peach', 'peach'),
   new StationColor('Orange', 'orange'),
   new StationColor('Yellow', 'yellow'),
+  new StationColor('Gold', 'gold'),
   new StationColor('Violet', 'violet'),
+  new StationColor('Dark Violet', 'dviolet'),
   new StationColor('Green', 'green'),
-  new StationColor('Blue', 'blue')
-
+  new StationColor('Dark Green', 'dgreen'),
+  new StationColor('Cyan', 'cyan'),
+  new StationColor('Blue', 'blue'),
+  new StationColor('Dark Blue', 'dblue')
 ];
--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -13,12 +13,19 @@ export class UiService {
   getColorHex(colorStr: string): string {
     const colorMap = {
       'white': '#ffffff',
-      'red': '#f9d9cf',
+      'pink': '#ffacb6',
+      'peach': '#fdc68d',
+      'red': '#e27878',
+      'cyan': '#50e9e9',
       'blue': '#9fd5ff',
+      'dblue': '#4795d1',
       'green': '#dff8ca',
+      'dgreen': '#a2e569',
       'yellow': '#fff1a3',
+      'gold': '#ffd700',
       'violet': '#e9b1e9',
-      'orange': '#ffd484',
+      'dviolet': '#e46ce4',
+      'orange': '#ffa500',
       'gray': '#d5d5d5',
       'dgray': '#787878'
     } as any;
